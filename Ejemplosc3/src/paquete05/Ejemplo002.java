b8d5efba81194e14ccb3f75f7a1027ec44ee3515
Anthony Joel <romeroyguanaanthonyjoel@gmail.com>
2023-12-20 16:10:11 -0500

    C7

--- Ejemplosc3/src/paquete05/Ejemplo002.java
+++ Ejemplosc3/src/paquete05/Ejemplo002.java
@@ -11,20 +11,24 @@ package paquete05;
 public class Ejemplo002 {
 
     public static void main(String[] args) {
-        
-     int[][] arreglo1 = {{10, 41, 40}, {1, 2, 3},{1,12,4}};
-     int suma=0;
-      
-        
+
+        int[][] arreglo1 = {{10, 41, 40}, {1, 2, 3}, {1, 12, 4}};
+        int suma = 0;
+
         for (int fila = 0; fila < arreglo1.length; fila++) {
             for (int col = 0; col < arreglo1[fila].length; col++) {
-                int c=0;
-               if (arreglo1[fila][col]== 10 ||arreglo1[fila][col]==2 || arreglo1[fila][col]==4){
-                 suma=suma+arreglo1[fila][col];
-               }
+                int c = 0;
+                if (arreglo1[fila][col] == 10 || arreglo1[fila][col] == 2 || arreglo1[fila][col] == 4) {
+                    suma = suma + arreglo1[fila][col];
+                }
             }
         }
         System.out.println(suma);
-     
+        /*   
+     Lo que se realizo en este codigo fue que utilizando los ciclos for 
+     recorremos el arreglo creado y filtramos usando una condicional 
+     if para sumar los numero 10,2 y 4 para al final presentar el resultado
+     de esa suma fuera de los ciclos for
+         */
     }
 }
