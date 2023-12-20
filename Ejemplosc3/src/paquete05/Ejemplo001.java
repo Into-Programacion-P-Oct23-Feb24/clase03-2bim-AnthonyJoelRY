4cadd827c816e0d89c358befe9375e5286f408e8
Anthony Joel <romeroyguanaanthonyjoel@gmail.com>
2023-12-20 15:08:46 -0500

    C1

---
+++ Ejemplosc3/src/paquete05/Ejemplo001.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package paquete05;
+
+/**
+ *
+ * @author LAB.ELECT
+ */
+public class Ejemplo001 {
+     public static void main(String[] args) {
+         
+     }
+}
